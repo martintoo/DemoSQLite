520e7b52e27583fafe28929e61e9c3cc6606023d
Ernesto Martin Torres Garcia <tinto.volk22@gmail.com>
2018-01-14 15:30:41 -0500

    Persistencia de datos en los Activities

--- app/src/main/java/digital/escuela/notepad/com/basededatos/MainActivity.java
+++ app/src/main/java/digital/escuela/notepad/com/basededatos/MainActivity.java
@@ -2,12 +2,51 @@ package digital.escuela.notepad.com.basededatos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    /*
+    EditText usuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        usuario=(EditText) findViewById(R.id.usuar);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("user",usuario.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        usuario.setText(savedInstanceState.getString("user"));
+
+    }
+     */
 }
+
+/*
+A la hora de voltear el app se borra NO ES UN BUG
+Se crea una activity automaticamente de la nada
+https://developer.android.com/reference/android/app/Activity.html
+(Lo de colores son ESTADOS)
+Activity Launched
+Activity Running
+Activity Shutdown
+*/
+
